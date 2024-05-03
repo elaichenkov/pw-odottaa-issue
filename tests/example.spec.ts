823523886d9ada938ebc10ad5bfdb1d27162d778
Yevhen Laichenkov <elaichenkov@gmail.com>
2024-05-03 19:26:18 -0400

    update(ci): remove browser installation step. fix test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ import playwrightApiMatchers from 'odottaa';
 expect.extend(playwrightApiMatchers);
 
 test('has title', async ({ request }) => {
-  const response = await request.get('https://petstore.swagger.io/v2/pet/1');
+  const response = await request.get('https://petstore.swagger.io/v2/pet/10002');
 
-  await expect(response).toHaveStatusCode(200);
+  await expect(response).toHaveStatusCode(404);
 });
