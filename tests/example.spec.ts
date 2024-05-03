ccf765dc6940c0f947e0e22ad67f91fb13ba3c09
Yevhen Laichenkov <elaichenkov@gmail.com>
2024-05-03 19:17:40 -0400

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+import playwrightApiMatchers from 'odottaa';
+
+expect.extend(playwrightApiMatchers);
+
+test('has title', async ({ request }) => {
+  const response = await request.get('https://petstore.swagger.io/v2/pet/1');
+
+  await expect(response).toHaveStatusCode(200);
+});
